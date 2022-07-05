d38771789a6d0ec43e6029f5375eb5b9fc82a51c
Jevgeni <belan.evgenii@gmail.com>
2022-07-05 19:47:25 +0300

    Added: ExceptionHndler, TabbedPaneChangeListener, 2 tabs and scroll panes to a view.

--- View.java
+++ View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -46,12 +48,24 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane htmlTextScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", htmlTextScrollPane);
 
+        JScrollPane plainTextScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.add("Text", plainTextScrollPane);
+
+        tabbedPane.setPreferredSize(new Dimension(400,300));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
     }
-
-
 
     public void exit() {
         controller.exit();
+    }
+
+    public void selectedTabChanged() {
+
     }
 }
