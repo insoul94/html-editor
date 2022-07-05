bb2809c0ddbd4ee75df5da019c79d9da23d7ab98
Jevgeni <belan.evgenii@gmail.com>
2022-07-05 09:05:43 +0300

    Added: FrameListener and window initialization.

--- View.java
+++ View.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    // Component contains of 2 editor panes.
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    // Component for visual html editing.
+    private JTextPane htmlTextPane = new JTextPane();
+    // Component for plain html editing with tags and content.
+    private JEditorPane plainTextPane = new JEditorPane();
 
     public Controller getController() {
         return controller;
@@ -21,8 +29,27 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void initMenuBar() {
 
     }
+
+    public void initEditor() {
+
+    }
+
+
 
     public void exit() {
         controller.exit();
