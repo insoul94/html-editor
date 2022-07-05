fa3ee9bcfa4f5de3b9fd059123def709705458e8
Jevgeni <belan.evgenii@gmail.com>
2022-07-05 21:17:26 +0300

    Implemented: initMenuBar(), View()

--- View.java
+++ View.java
@@ -17,6 +17,16 @@ public class View extends JFrame implements ActionListener {
     // Component for plain html editing with tags and content.
     private JEditorPane plainTextPane = new JEditorPane();
 
+    public View()  {
+        super();
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
     public Controller getController() {
         return controller;
     }
@@ -44,7 +54,16 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, menuBar);
+        MenuHelper.initEditMenu(this, menuBar);
+        MenuHelper.initStyleMenu(this, menuBar);
+        MenuHelper.initAlignMenu(this, menuBar);
+        MenuHelper.initColorMenu(this, menuBar);
+        MenuHelper.initFontMenu(this, menuBar);
+        MenuHelper.initHelpMenu(this, menuBar);
 
+        getContentPane().add(menuBar, BorderLayout.NORTH);
     }
 
     public void initEditor() {
