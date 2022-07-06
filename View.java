81f21dc798eff162916dd717ad02da2046477de9
Jevgeni <belan.evgenii@gmail.com>
2022-07-06 12:36:29 +0300

    Impl-d: styling actions. Added: undo/redo handling.

--- View.java
+++ View.java
@@ -2,20 +2,30 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
-    // Component contains of 2 editor panes.
+    // Manages a list of undoable edits.
+    private UndoManager undoManager = new UndoManager();
+    // Listens to edits that can be undone.
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    // Main component contains of 2 editor tabs.
     private JTabbedPane tabbedPane = new JTabbedPane();
     // Component for visual html editing.
     private JTextPane htmlTextPane = new JTextPane();
     // Component for plain html editing with tags and content.
     private JEditorPane plainTextPane = new JEditorPane();
+
 
     public View()  {
         super();
@@ -33,6 +43,10 @@ public class View extends JFrame implements ActionListener {
 
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 
     @Override
@@ -86,5 +100,33 @@ public class View extends JFrame implements ActionListener {
 
     public void selectedTabChanged() {
 
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
     }
 }
