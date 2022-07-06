b3f253318f6a9f1a6e4c7dfeb68d32ddeef4c224
Jevgeni <belan.evgenii@gmail.com>
2022-07-06 14:30:46 +0300

    Added: Controller.get/setPlainText()

--- Controller.java
+++ Controller.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -48,4 +52,25 @@ public class Controller {
 
         view.update();
     }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+        return writer.toString();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader reader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(reader, document, 0);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
 }
